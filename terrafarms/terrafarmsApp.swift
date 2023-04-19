ea08a62003026caf261d850ac8d0de86f13af0c5
azzam ubaidillah <azzam.ubaidillah05@gmail.com>
2023-04-20 01:24:42 +0700

    Initial Commit

---
+++ terrafarms/terrafarmsApp.swift
@@ -0,0 +1,17 @@
+//
+//  terrafarmsApp.swift
+//  terrafarms
+//
+//  Created by azzam on 19/04/23.
+//
+
+import SwiftUI
+
+@main
+struct terrafarmsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LoginView()
+        }
+    }
+}
