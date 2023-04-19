b03520b5b1d1a2b4b2bf69ed6575b317bdfe3c15
azzam ubaidillah <azzam.ubaidillah05@gmail.com>
2023-04-20 06:02:19 +0700

    login page

--- terrafarms/LoginView.swift
+++ terrafarms/LoginView.swift
@@ -12,66 +12,66 @@ struct LoginView: View {
     @State private var password: String = ""
 
     var body: some View {
-        VStack {
-            Spacer()
-            Image("terrafarms-logo")
-            Spacer()
-            VStack(alignment: .leading) {
-                Text("Selamat datang di Terrafarms!").font(.custom("Urbanist-Medium", size: 20)).padding(.bottom, 4)
+        NavigationView {
+            VStack {
+                Spacer()
+                Image("terrafarms-logo")
+                Spacer()
+                VStack(alignment: .leading) {
+                    Text("Selamat datang di Terrafarms!").font(.custom("Urbanist-Medium", size: 20)).padding(.bottom, 4)
 
-                Text("Silahkan masuk dengan akun anda").font(.custom("Urbanist", size: 13))
+                    Text("Silahkan masuk dengan akun anda").font(.custom("Urbanist", size: 13))
 
-                TextField("Email", text: $email).font(.custom("Urbanist", size: 13))
-                    .autocapitalization(.none)
-                    .disableAutocorrection(true)
-                    .padding()
-                    .background(Color(.secondarySystemBackground))
-                    .cornerRadius(8).padding(.top)
-
-                SecureField("Password", text: $password).font(.custom("Urbanist", size: 13)).padding().background(Color(.secondarySystemBackground))
-                    .cornerRadius(8)
-
-                Button(action: {
-                    // Perform login action
-                }) {
-                    Text("Masuk").font(.custom("Urbanist-Medium", size: 16))
-                        .foregroundColor(.white)
-                        .font(.headline)
+                    TextField("Email", text: $email).font(.custom("Urbanist", size: 13))
+                        .autocapitalization(.none)
+                        .disableAutocorrection(true)
                         .padding()
-                        .frame(maxWidth: .infinity)
-                        .background(Color.accentColor)
+                        .background(Color(.secondarySystemBackground))
+                        .cornerRadius(8).padding(.top)
+
+                    SecureField("Password", text: $password).font(.custom("Urbanist", size: 13)).padding().background(Color(.secondarySystemBackground))
                         .cornerRadius(8)
-                }
-                .padding(.top, 16)
-                Button(action: {
-                    // Perform login action
-                }) {
-                    Text("Masuk dengan Google").font(.custom("Urbanist-Medium", size: 16))
-                        .foregroundColor(.accentColor)
-                        .font(.headline)
-                        .padding()
-                        .frame(maxWidth: .infinity)
-                        .background(Color.white)
-                        .cornerRadius(8).overlay(
-                            RoundedRectangle(cornerRadius: 8)
-                                .stroke(Color.accentColor)
-                        )
-                }
-                .padding(.top, 8)
-                
-                
-            }.padding(.horizontal)
 
-            Spacer()
-            HStack{
-                Text("Belum mempunyai akun?").font(.custom("Urbanist", size: 14))
-                Button(action: {
-                    // Perform login action
-                }){
-                    Text("Daftar").font(.custom("Urbanist", size: 14))
+                    Button(action: {
+                        // Perform login action
+                    }) {
+                        Text("Masuk").font(.custom("Urbanist-Medium", size: 16))
+                            .foregroundColor(.white)
+                            .font(.headline)
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .background(Color.accentColor)
+                            .cornerRadius(8)
+                    }
+                    .padding(.top, 16)
+                    Button(action: {
+                        // Perform login action
+                    }) {
+                        Text("Masuk dengan Google").font(.custom("Urbanist-Medium", size: 16))
+                            .foregroundColor(.accentColor)
+                            .font(.headline)
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .background(Color.white)
+                            .cornerRadius(8).overlay(
+                                RoundedRectangle(cornerRadius: 8)
+                                    .stroke(Color.accentColor)
+                            )
+                    }
+                    .padding(.top, 8)
+
+                }.padding(.horizontal)
+
+                Spacer()
+                HStack {
+                    Text("Belum mempunyai akun?").font(.custom("Urbanist", size: 14))
+
+                    NavigationLink(destination: RegisterView().navigationBarBackButtonHidden(true)) {
+                        Text("Daftar").font(.custom("Urbanist", size: 14))
+                    }
                 }
+                Spacer()
             }
-            Spacer()
         }
     }
 }
