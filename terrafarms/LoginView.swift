7c9cc27dfe3f3d033cc9657a305e3cf246af44d5
azzam ubaidillah <azzam.ubaidillah05@gmail.com>
2023-04-23 00:51:28 +0700

    login check

--- terrafarms/LoginView.swift
+++ terrafarms/LoginView.swift
@@ -5,16 +5,15 @@
 //  Created by azzam on 19/04/23.
 //
 
-import SwiftUI
 import Firebase
 import FirebaseAuth
-
+import SwiftUI
 
 struct LoginView: View {
     @State private var email: String = ""
     @State private var password: String = ""
     @State private var isSignedIn = false
-    
+
     var body: some View {
         NavigationView {
             if isSignedIn {
@@ -26,21 +25,21 @@ struct LoginView: View {
                     Spacer()
                     VStack(alignment: .leading) {
                         Text("Selamat datang di Terrafarms!").font(.custom("Urbanist-Medium", size: 20)).padding(.bottom, 4)
-    
+
                         Text("Silahkan masuk dengan akun anda").font(.custom("Urbanist", size: 13))
-    
+
                         TextField("Email", text: $email).font(.custom("Urbanist", size: 13))
                             .autocapitalization(.none)
                             .disableAutocorrection(true)
                             .padding()
                             .background(Color(.secondarySystemBackground))
                             .cornerRadius(8).padding(.top)
-    
+
                         SecureField("Password", text: $password).font(.custom("Urbanist", size: 13)).padding().background(Color(.secondarySystemBackground))
                             .cornerRadius(8)
-    
+
                         Button(action: {
-                            Auth.auth().signIn(withEmail: email, password: password) { result, error in
+                            Auth.auth().signIn(withEmail: email, password: password) { _, error in
                                 if let error = error {
                                     // Handle sign in error
                                     print("Sign in error: \(error.localizedDescription)")
@@ -61,28 +60,34 @@ struct LoginView: View {
                         }
                         .padding(.top, 16)
                         Button(action: {
-                           print("login dengan google")
-    
+                            print("login dengan google")
+
                         }) {
-                            Text("Masuk dengan Google").font(.custom("Urbanist-Medium", size: 16))
-                                .foregroundColor(.accentColor)
-                                .font(.headline)
-                                .padding()
-                                .frame(maxWidth: .infinity)
-                                .background(Color.white)
-                                .cornerRadius(8).overlay(
-                                    RoundedRectangle(cornerRadius: 8)
-                                        .stroke(Color.accentColor)
-                                )
+                            HStack {
+                                Image("google-logo")
+                                    .resizable()
+                                    .frame(width: 24, height: 24)
+                                Text("Masuk dengan Google")
+                                    .font(.custom("Urbanist-Medium", size: 16))
+                            }
+                            .foregroundColor(.accentColor)
+                            .font(.headline)
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .background(Color.white)
+                            .cornerRadius(8).overlay(
+                                RoundedRectangle(cornerRadius: 8)
+                                    .stroke(Color.accentColor)
+                            )
                         }
                         .padding(.top, 8)
-    
+
                     }.padding(.horizontal)
-    
+
                     Spacer()
                     HStack {
                         Text("Belum mempunyai akun?").font(.custom("Urbanist", size: 14))
-    
+
                         NavigationLink(destination: RegisterView().navigationBarBackButtonHidden(true)) {
                             Text("Daftar").font(.custom("Urbanist", size: 14))
                         }
@@ -93,8 +98,6 @@ struct LoginView: View {
         }
     }
 }
-
-
 
 struct LoginView_Previews: PreviewProvider {
     static var previews: some View {
